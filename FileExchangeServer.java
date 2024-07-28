ccd8b8cb6220da0ec7b98946f4e929410eb98583
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-07-28 14:11:54 +0800

    created fileexchangeserver

---
+++ FileExchangeServer.java
@@ -0,0 +1,19 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class FileExchangeServer {
+    
+    public static void main(String[] args) {
+        
+        int nPort = 4000;
+        ServerSocket serverSocket;
+        Socket serverEndpoint;
+        try{
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}

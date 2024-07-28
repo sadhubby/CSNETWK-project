77184825e7deb740cd3678f7ad8c6104fd2c2f5e
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-07-29 03:52:47 +0800

    latest push. check comments.

--- FileExchangeClient.java
+++ FileExchangeClient.java
@@ -7,13 +7,15 @@ public class FileExchangeClient {
     private static String serverAddress;
     private static int port;
     private static Socket clientEndpoint;
+    private static String username;
+    private static String filename;
     public static void main(String[] args) {
 
         int join = 0;
         int main = 0;
         Scanner sc = new Scanner(System.in);
         try{
-            do{
+            do{ //loop for joining a server first
                 System.out.println("\nPlease join a server:");
                 String inputConnect = sc.nextLine();
                 if (inputConnect.startsWith("/join")){
@@ -21,12 +23,10 @@ public class FileExchangeClient {
                     if(parts.length == 2){
                         serverAddress = parts[0];
                         port = Integer.parseInt(parts[1]);
-                        //clientEndpoint = new Socket(serverAddress, port);
-                        System.out.println("Connection to the File Exchange\r" + 
-                                                        "Server is successful!");     
+                        clientEndpoint = new Socket(serverAddress, port);
+                        System.out.println("Connection is successful!");  
                         join = 1;                                                   
                     }  
-                    
                 }
                 if (inputConnect.startsWith("/?")){
                     System.out.println("----- Here are the commands -----");
@@ -34,14 +34,54 @@ public class FileExchangeClient {
                 }
             }while(join == 0);
 
-            do{
+            do{ // loop after joining a server.
+
                 System.out.println("\nPlease enter command:");
                 String inputCommand = sc.nextLine(); 
+
+                // leave function
                 if (inputCommand.startsWith("/leave")){
-                    System.out.println("Thank you for using this File Exchange program");
+                    System.out.println("Connection closed. Thank you!");
                     main = 1;
-                    //clientEndpoint.close();
+                    clientEndpoint.close();
                 }
+
+                // register function
+                if (inputCommand.startsWith("/register")){
+                    
+                    String[] parts = inputCommand.substring(10).split(" ");
+                    if(parts.length == 1){
+                        username = parts[0];
+                    }
+                    System.out.println("Welcome " + username + "!");
+                }
+
+                // store a file into server function
+                if (inputCommand.startsWith("/store")){
+                    String[] parts = inputCommand.substring(7).split(" ");
+                    if(parts.length == 1){
+                        filename = parts[0];
+                    }
+                    System.out.println(username + " uploaded " + filename);
+                }
+
+                // directory function
+                if (inputCommand.startsWith("/dir")){
+                    System.out.println("Server directory");
+                    // print out array of files 
+                }
+
+                // get function
+                if (inputCommand.startsWith("/get")){
+                    String[] parts = inputCommand.substring(5).split(" ");
+                    if(parts.length == 1){
+                        filename = parts[0];
+                        //check if the filename DOES exist
+                    }
+                    System.out.println("File received from Server: " + filename);
+                }
+
+                // list of commands function
                 if (inputCommand.startsWith("/?")){
                     System.out.println("----- Here are the commands -----");
                     System.out.println("To leave : /leave");
--- FileExchangeServer.java
+++ FileExchangeServer.java
@@ -3,17 +3,62 @@ import java.io.*;
 import java.util.*;
 
 public class FileExchangeServer {
+
     
     public static void main(String[] args) {
-        
-        int nPort = 4000;
-        ServerSocket serverSocket;
-        Socket serverEndpoint;
-        try{
+		int nPort = Integer.parseInt(args[0]); // 4000
+		System.out.println("Server: Listening on port " + args[0] + "...");
+		ServerSocket serverSocket;
+		Socket serverEndpoint;
 
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+		try 
+		{
+			serverSocket = new ServerSocket(nPort);
+			serverEndpoint = serverSocket.accept();
+			
+			System.out.println("\nServer: New client connected: " + serverEndpoint.getRemoteSocketAddress());
+			
+			DataInputStream disReader = new DataInputStream(serverEndpoint.getInputStream());
+		
+			DataOutputStream dosWriter = new DataOutputStream(serverEndpoint.getOutputStream());	
 
-    }
+			FileWriter fileWriter = new FileWriter("Download.txt");
+			fileWriter.write("Hello World!");
+			fileWriter.close();
+
+            File file = new File("Download.txt"); 
+            FileInputStream fis = new FileInputStream(file); 
+            
+            byte[] buffer = new byte[(int) file.length()]; 
+			fis.read(buffer); 
+
+            dosWriter.writeInt(buffer.length); 
+            dosWriter.write(buffer, 0, buffer.length);
+			System.out.println("\nServer: Sending File \"Download.txt\" (" + buffer.length +" bytes)\n");
+            fis.close();
+
+
+
+
+
+
+
+
+
+
+
+
+            
+			serverEndpoint.close();
+            serverSocket.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			System.out.println("Server: Connection is terminated.");
+		}
+	}
 }
